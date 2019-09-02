244a0ecc6c76d4fb34179b433aacb56555d819d4
Your Name <jardo_atmai@yahoo.com>
2019-09-02 16:39:43 -0400

    Intervals update growth status of plants that are planted

--- src/PlantTile.vue
+++ src/PlantTile.vue
@@ -13,27 +13,62 @@ export default {
   },
   data() {
     return {
-        square: {
-            x: this.plantInfo.x,
-            y: this.plantInfo.y,
-            height: 20,
-            width: 20,
-            fill: "white",
-            stroke: "green",
-            strokeWidth: .5
-        },
+      interval: null,
+      maturity: "germ",
+      count: 0,
+      square: {
+        x: this.plantInfo.x,
+        y: this.plantInfo.y,
+        height: 20,
+        width: 20,
+        fill: "white",
+        stroke: "green",
+        strokeWidth: 0.5
+      },
 
-        plant: {
-            x: this.plantInfo.x,
-            y: this.plantInfo.y,
-            fontSize: 15,
-            text: "p"
-        }
-        }
+      plant: {
+        x: this.plantInfo.x,
+        y: this.plantInfo.y,
+        fontSize: 15,
+        text: "p",
+        stroke: "black"
+      }
+    };
   },
 
   mounted() {
     console.log("PlantTile Mounted");
+    console.log("maturity:", this.maturity);
+
+    this.interval = setInterval(() => {
+      this.growPlant();
+    }, 1000);
+
+  },
+
+  beforeDestroy() {
+    clearInterval(this.interval);
+  },
+
+  methods: {
+    growPlant() {
+      this.count += 1;
+
+      switch(this.count){
+        case 10:
+          this.maturity = "seedling";
+        case 20:
+          this.maturity = "mature";
+        case 30:
+          this.maturity = "ideal";
+        case 40:
+          this.maturity = "dead";
+      }
+
+      console.log("maturity:", this.maturity);
+      console.log("interval:", this.count);
+      // clearInterval(this.interval);
+    }
   }
 };
 </script>
